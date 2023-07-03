569ed06861a25240321825283e8c6719a20bacc8
ibrahim mustapha <ibrahimmustapha@ibrahims-MacBook-Pro.local>
2023-07-03 20:58:11 +0100

    assignment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-price = [567 , 300 , 338 , 590 , 689 , 794, 836 , 496 , 479 , 433 , 862 , 367 , 678 , 126 , 561 , 374 , 321 , 747 , 959 , 799]
-for numbers in price:
-    x = numbers+ 100
-    print("the value of price is "  +  str(x))
-
+#Code to find even and odd number
+numbers= {1,2,3,4,5,6,7,8,9,10,11,12,14,16,88}
+for items in numbers:
+    if items % 2 == 0:
+       print(f"{items} is an even number")
+    else:
+        print(f"{items} is an odd number")
